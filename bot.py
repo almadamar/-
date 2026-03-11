94715d8acd8a0a8cc93a9b23e865ac0056fbbfef
kmr <al.karbalae56@gmail.com>
2026-03-11 06:53:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,8 @@
 import os, importlib, asyncio
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
-
-TOKEN = "6099646606:AAHu-znvZ9bawGNl4autKn3YcMXSrxz4NzI"
-DB_FILE = "users.txt"
-DOWNLOAD_DIR = "downloads"
+from telegram.ext import ApplicationBuilder
+from config_data import TOKEN, DOWNLOAD_DIR
 
 if not os.path.exists(DOWNLOAD_DIR): os.makedirs(DOWNLOAD_DIR)
-
-def register_user(user_id):
-    if not os.path.exists(DB_FILE): open(DB_FILE, 'w').close()
-    with open(DB_FILE, 'r') as f: users = f.read().splitlines()
-    if str(user_id) not in users:
-        with open(DB_FILE, 'a') as f: f.write(f"{user_id}\n")
-
-async def track_and_save(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_user:
-        register_user(update.effective_user.id)
 
 async def post_init(application):
     plugins = ['plugin_pro', 'plugin_youtube', 'plugin_audio', 'plugin_extras']
@@ -29,8 +15,6 @@ async def post_init(application):
             print(f"❌ Error {plugin}: {e}")
 
 if __name__ == '__main__':
-    # استخدام تقنيات الـ Async لتحسين الاستجابة
     app = ApplicationBuilder().token(TOKEN).post_init(post_init).build()
-    app.add_handler(MessageHandler(filters.ALL, track_and_save), group=-1)
-    print("--- BOT IS RUNNING FAST ---")
+    print("--- BOT IS STARTING ---")
     app.run_polling()
