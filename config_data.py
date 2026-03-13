55acc33dff437a6fd88d09aeaead71eb4bc764d4
kmr <al.karbalae56@gmail.com>
2026-03-13 06:32:55 +0300

    Update config_data.py

--- config_data.py
+++ config_data.py
@@ -1,4 +1,8 @@
+# إعدادات البوت والبيانات الخاصة بك
 TOKEN = "6099646606:AAHu-znvZ9bawGNl4autKn3YcMXSrxz4NzI"
 OWNER_ID = 162459553
 CHANNEL_LINK = "https://t.me/+nBVM5qNb2uphMzUy"
 DOWNLOAD_DIR = "downloads"
+
+# الرابط السحابي الخاص بك (MongoDB)
+MONGO_URI = "mongodb+srv://alkarbalae56_db_user:agwAozOQkWO7nltP@cluster0.69ok5zw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
