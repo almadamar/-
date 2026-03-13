c557a35581123791e498ee0e621d85bfd1ee3413
kmr <al.karbalae56@gmail.com>
2026-03-13 21:00:31 +0300

    Update config_data.py

--- config_data.py
+++ config_data.py
@@ -1,8 +1,13 @@
-# إعدادات البوت والبيانات الخاصة بك
+# بوت التحميل @Down2024_bot
 TOKEN = "6099646606:AAHu-znvZ9bawGNl4autKn3YcMXSrxz4NzI"
-OWNER_ID = 162459553
-CHANNEL_LINK = "https://t.me/+nBVM5qNb2uphMzUy"
-DOWNLOAD_DIR = "downloads"
 
-# الرابط السحابي الخاص بك (MongoDB)
-MONGO_URI = "mongodb+srv://alkarbalae56_db_user:agwAozOQkWO7nltP@cluster0.69ok5zw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# بوت الأرشفة @AutoMusicHubBot
+SECOND_TOKEN = "8044419232:AAHCxEyw-mHBz-Vu9pTrihA8PfjcVODPU5E"
+MUSIC_STORAGE_ID = -1001078117510  # قناة @Musiciqh لتخزين الصوتيات فقط
+
+# القناة الرسمية (مرجع الاشتراك الإجباري للبوتين معاً)
+REQUIRED_CHANNEL_ID = -1003773995399  # القناة التي يجب الاشتراك بها أولاً
+CHANNEL_LINK = "https://t.me/+nBVM5qNb2uphMzUy"
+
+OWNER_ID = 162459553
+DOWNLOAD_DIR = "downloads"
